ba9cbfde37137d22e0b28a9fd8ceb0c1cef0d33a
ffffkewon <dltjsdn24@gmail.com>
2022-02-23 02:38:11 +0900

    mnist 책버전

---
+++ condatest.py
@@ -0,0 +1,36 @@
+import time
+from keras.datasets import mnist
+(train_images,train_labels),(test_images,test_labels)=mnist.load_data()
+
+#%%network
+from keras import models
+from keras import layers
+
+network = models.Sequential()
+network.add(layers.Dense(512,activation='relu',input_shape=(28*28,)))
+network.add(layers.Dense(10,activation='softmax'))
+
+#%%compiler
+network.compile(optimizer='rmsprop',
+                loss = 'categorical_crossentropy',
+                metrics=['accuracy'])
+#%%Data processing
+train_images = train_images.reshape((60000,28*28)) 
+train_images = train_images.astype('float32') /255 #unit to float + normalize
+
+test_images = test_images.reshape((10000,28*28))
+test_images = test_images.astype('float32') /255
+#%%One hot encoding
+from keras.utils import to_categorical
+
+train_labels = to_categorical(train_labels)
+test_labels = to_categorical(test_labels)
+
+#%% Learning
+network.fit(train_images,train_labels,epochs=5,batch_size=128)
+
+#%% Validate
+print("학습된 신경망을 검증합니다...")
+time.sleep(5)
+test_loss,test_acc= network.evaluate(test_images,test_labels)
+print(test_loss, test_acc)
